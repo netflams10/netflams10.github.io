80280ced6ba0301609abc36806f142d9cfc5070d
netflams <segope44@gmail.com>
2024-07-15 18:24:37 +0100

    update: load on smaller screens

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
-  <Theme />
+  <div class="mobile_not_present" v-if="is_mobile">
+    Currently, there is no design available for smaller screens. I kindly ask
+    for your understanding as my expertise is primarily in backend
+    development😏🥲.
+  </div>
+  <Theme v-else />
 </template>
 
-<script setup>
+<script>
 import Theme from "./theme/Theme.vue";
+
+export default {
+  components: { Theme },
+  data() {
+    return { is_mobile: false };
+  },
+  created() {
+    const check =
+      /Andoird|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      ) || window.innerWidth <= 768;
+    this.is_mobile = check;
+  },
+};
 </script>
